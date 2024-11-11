7cc38ce64d122ab3e480997c4390de7fe8d3e571
Xojiakbar <hojiakbarandaqulov5@gmail.com>
2024-11-11 20:21:58 +0500

    bean create fix

--- src/main/java/com/example/dto/category/CategoryCreateDTO.java
+++ src/main/java/com/example/dto/category/CategoryCreateDTO.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import lombok.NonNull;
-
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CategoryCreateDTO {
     @NotBlank(message = "category name is required")
     private String name;
+
+    @NotBlank(message = ",,,,,,,,,,,,,,,,")
+    private String description;
 }
